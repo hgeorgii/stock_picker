dfad2e20217923c92468ce918e91cdc52a7a407e
Hans Georgii <georgii.hans@gmail.com>
2020-02-11 18:53:23 +0100

    Changed some syntax

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,34 +2,31 @@
 # It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
 
 class Stock
-  attr_accessor :stocks
-
-  def initialize(stocks)
-    @stocks = stocks
-  end
-
-  def stock_picker
+  def stock_picker(stocks)
     highest_profit = 0
     best_day = []
 
-    stocks.each_with_index do |x, index|
-      unless index == 0
-        count = index
-        until stocks[count] == stocks.first
-          current_profit = x - stocks[count - 1]
-          if current_profit > highest_profit
-            highest_profit = current_profit
-            best_day = [count - 1, index]
-          end
-          count -= 1
+    stocks.each_with_index do |current_price, current_day|
+      next if current_day == 0
+
+      count = current_day
+      until count == 0
+        current_profit = current_price - stocks[count - 1]
+
+        if current_profit > highest_profit
+          highest_profit = current_profit
+          best_day = [count - 1, current_day]
         end
+
+        count -= 1
       end
     end
-    p best_day
+
+    best_day
   end
 end
 
 #s = Stocker.new([1,2,3,4,5,20])
-s = Stock.new([17,3,6,9,15,8,6,1,10])
+s = Stock.new
 #s = Stocker.new([7,1,2,20,3,4])
-s.stock_picker
+p s.stock_picker([17,3,6,9,15,8,6,1,10])
